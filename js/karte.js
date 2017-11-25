7659d18edf12dddb86e141b2688a4a8a3541e9ad
astoorangi <astoorangi@outlook.com>
2017-11-25 16:31:37 +0100

    Added ajax

--- js/karte.js
+++ js/karte.js
@@ -14,16 +14,23 @@ function mark(color, coar, radius) {
 function showNewCycles(coar) {
     //[ [52.541806665759616,13.549575805664064] [3,4] ]
     var url = "https://www.overpass-api.de/api/interpreter?data=[out:json];area[%22boundary%22~%22administrative%22][%22name%22~%22Berlin%22];node(area)[%22amenity%22~%22police%22];out;";
+    console.log(url);
     $.ajax({
         url: url
     })
         .done(function (data) {
+            console.log(data);
             data.elements.forEach(function(place) {
-
+                mark('#0000FF',[place.lat,place.lon],500);
             })
+        })
+        .fail(function(e,textstatus) {
+            console.log(e);
+            console.log(textstatus);
         });
 }
 
 map.on("zoomend", function() {
-    showNewCycles();
+    a = map.getBounds();
+    /*showNewCycles();*/
 });
