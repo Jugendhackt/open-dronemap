6770b6a0af4bbf3ac71b7b1a7ae84945922de147
astoorangi <astoorangi@outlook.com>
2017-11-25 11:51:27 +0100

    exported js

---
+++ js/karte.js
@@ -0,0 +1,5 @@
+var map = L.map(document.getElementById("map"), {
+    center: [52.5025, 13.4122],
+    zoom: 13,
+});
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
