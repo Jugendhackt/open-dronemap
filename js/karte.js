1e1c4a6b500486d1b9b42b9028bc15e84f39dcec
astoorangi <astoorangi@outlook.com>
2017-11-25 19:48:19 +0100

    Added Comments

--- js/karte.js
+++ js/karte.js
@@ -1,31 +1,31 @@
-var map = L.map(document.getElementById("map"), {
-    center: [52.5025, 13.4122],
+var map = L.map(document.getElementById("map"), {   // Erstelle Karte
+    center: [52.5025, 13.4122],     // Karte zentriert auf Betahaus Berlin
     zoom: 13,
 });
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {    // Definiere Tiles (OSM)
     foo: 'bar',
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap-Mitwirkende</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap-Mitwirkende</a>',  // OSM Copyright Hinweis
 }).addTo(map);
 
-function mark(color, coar, radius) {
-    L.circle(coar, { radius: radius, color: color }).addTo(map);
+function mark(color, coar, radius) {        // function, um Gebäude mit Kreis zu markieren
+    L.circle(coar, { radius: radius, color: color }).addTo(map);    // Erstelle Kreis
 }
 
-function showNewCycles() {
-    var bounds = map.getBounds();
-    var coar = [bounds._southWest.lat, bounds._southWest.lng, bounds._northEast.lat, bounds._northEast.lng] //definiert screenposition
-    var roundedcoar = coar.map(function (k) {
+function showNewCycles() {          // Suche Gebäude
+    var bounds = map.getBounds();   // bounds = Eckpunkte der Karte
+    var coar = [bounds._southWest.lat, bounds._southWest.lng, bounds._northEast.lat, bounds._northEast.lng] // Definiert screenposition
+    var roundedcoar = coar.map(function (k) {   // Runde Eckkoordinaten auf 4 nachkommastellen
         return (Math.round(k * 10000) / 10000)
     });
-    var url = "https://www.overpass-api.de/api/interpreter?data=[out:json];node(" + roundedcoar.join(",") + ")[%22amenity%22~%22police%22];out;"; //ruft elemente für bound-koordinaten auf
+    var url = "https://www.overpass-api.de/api/interpreter?data=[out:json];node(" + roundedcoar.join(",") + ")[%22amenity%22~%22police%22];out;"; // Erstelle URL für Ajax
     console.log(url);
     $.ajax({
         url: url
     })
         .done(function (data) {
             console.log(data);
-            data.elements.forEach(function (place) {
-                mark('#FF0000', [place.lat, place.lon], 500);
+            data.elements.forEach(function (place) {    // Jeder einzelne Gebäude
+                mark('#FF0000', [place.lat, place.lon], 500);   // Erstelle Kreis mit function mark
             })
         })
         .fail(function (e, textstatus) {
@@ -34,11 +34,11 @@ function showNewCycles() {
         });
 }
 
-map.on("zoomend", function () {
+map.on("zoomend", function () { // Aktualisiere markierte Gebäude bei Zoomen 
     showNewCycles();
 });
 
-map.on("moveend", function () {
+map.on("moveend", function () { // Aktualisiere markierte Gebäude bei Verschieben
     showNewCycles();
 });
 
