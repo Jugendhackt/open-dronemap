813fbd6ca3fcb5d8abf332c78f56ba29a89d8b51
astoorangi <astoorangi@outlook.com>
2017-11-25 14:26:33 +0100

    added mark()

--- js/karte.js
+++ js/karte.js
@@ -6,4 +6,7 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
     foo: 'bar',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap-Mitwirkende</a>',
 }).addTo(map);
-L.circle([52.5025, 13.4122], { radius: 500, color: "#ffffff" }).addTo(map);
+
+function mark(color, coar, radius) {
+    L.circle(coar, { radius: radius, color: color }).addTo(map);
+}
