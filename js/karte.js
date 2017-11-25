8483d9cbca516b5052e300f4ecc989d3c2b0c576
IFoundAnotherUsername <atzeje@web.de>
2017-11-25 13:05:19 +0100

    added Zusatzinfos

--- js/karte.js
+++ js/karte.js
@@ -3,3 +3,4 @@ var map = L.map(document.getElementById("map"), {
     zoom: 13,
 });
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
+L.circle([52.5025, 13.4122], {radius: 500, color: "#ffffff"}).addTo(map);
