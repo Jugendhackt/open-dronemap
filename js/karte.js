9dcde30bfd824782307fb32099079fa5f4772106
astoorangi <astoorangi@outlook.com>
2017-11-25 14:05:26 +0100

    Added osm license to map

--- js/karte.js
+++ js/karte.js
@@ -2,5 +2,8 @@ var map = L.map(document.getElementById("map"), {
     center: [52.5025, 13.4122],
     zoom: 13,
 });
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
-L.circle([52.5025, 13.4122], {radius: 500, color: "#ffffff"}).addTo(map);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+    foo: 'bar',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap-Mitwirkende</a>',
+}).addTo(map);
+L.circle([52.5025, 13.4122], { radius: 500, color: "#ffffff" }).addTo(map);
