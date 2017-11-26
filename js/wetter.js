6519e2379306b057ea78bf42b61356ebb6bea543
IFoundAnotherUsername <atzeje@web.de>
2017-11-26 09:56:31 +0100

    wetter

--- js/wetter.js
+++ js/wetter.js
@@ -1,3 +1,9 @@
 if (navigator.geolocation) { 
-    
+    var coords = navigator.geolocation.getCurrentPosition();
+    $("#location").html(coords)
+}
+else{
+    $("#msg").html(
+        "Ihr Webbrowser unterstützt leider keine Geolocation. Deshalb kann das Wetter für sie nicht ortsspezifisch ermittelt werden."
+    )
 }
