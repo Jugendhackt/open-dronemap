33c598fa4b335d4c9c5fbb7870445d891605afba
IFoundAnotherUsername <atzeje@web.de>
2017-11-26 11:02:41 +0100

    wetter beendet

--- js/wetter.js
+++ js/wetter.js
@@ -1,10 +1,22 @@
 if (navigator.geolocation) { 
     navigator.geolocation.getCurrentPosition(function(pos) {
-        $("#msg").html(pos.coords.latitude + " " + pos.coords.longitude)
+        $("#msg").html("Ihre Position: " + pos.coords.latitude + " " + pos.coords.longitude);
+        console.log(pos);
+        var lat = pos.coords.latitude
+        var lng = pos.coords.longitude
+        lat = Math.round(lat * 10000)/10000
+        lng = Math.round(lng * 10000)/10000
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=f5a1b2e0d2a5694785c81addd5465159"
+        })
+        .done(function(data){
+            var speed = data.wind.speed;
+            $("#msg").html("Die Windwgeschwindigkeit beträgt: " + speed);
+        })
     });
 }
 else{
     $("#msg").html(
         "Ihr Webbrowser unterstützt leider keine Geolocation. Deshalb kann das Wetter für sie nicht ortsspezifisch ermittelt werden."
-    )
+    );
 }
